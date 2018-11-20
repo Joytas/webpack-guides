98ea0f5362a506eabbcbb22fa2c384a1c163519f
Joytas <tusmanchook@o2.pl>
2018-11-20 23:34:38 +0100

    03-Asset Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import _ from "lodash";
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
     let element = document.createElement('div');
 
+    //lodash
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    //css
+    element.classList.add('hello');
+
+    //images
+    let myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+    //xml
+    console.log(Data);
 
     return element;
 }
